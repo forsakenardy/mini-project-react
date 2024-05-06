a450cdb8eae5f32da233e8b995fba39c0d9e9510
nisol <nisolmedinaue@gmail.com>
2024-05-07 00:40:32 +0200

    footer,button,color

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,15 +1,31 @@
 import "../Styles/Sidebar.css";
+import FacebookIcon from "../assets/images/facebook.png";
+import InstaIcon from "../assets/images/instagram.png";
+import twitterIcon from "../assets/images/twitterX.png";
 
 function Sidebar() {
-    return (
-        <>
-        <div className="Sidebar-Content">
-            <h1>Home</h1>
-            <h1>About</h1>
-        </div>
-        <div className="Footer"></div>
-        </>
-    )
+  return (
+    <>
+      <div className="Sidebar-Content">
+        <h1>Home</h1>
+        <h1>About</h1>
+      </div>
+      <div className="Footer">
+        <footer>
+          <div className="descriptionP">
+            &copy; 2024 Developed by Arnaldo, Mikel And Nisol
+          </div>
+
+          <ul className="socialMediaIcons">
+            <img src={FacebookIcon} alt="" />
+            <img src={InstaIcon} alt="" />
+            <img src={twitterIcon} alt="" />
+            <button className="btnGithub">githublink</button>
+          </ul>
+        </footer>
+      </div>
+    </>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
