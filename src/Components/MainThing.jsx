78f12217750db359595b5929105513d4aff07176
Mikel Jimenez <majc1907@gmail.com>
2024-05-08 17:41:06 +0200

    0805-mikel

--- src/Components/MainThing.jsx
+++ src/Components/MainThing.jsx
@@ -3,31 +3,33 @@ import jsonArray from "../recipes.json"
 import { useParams, Link } from "react-router-dom"
 import { useState } from "react";
 
-function MainThing() {
-    const [recipes, setRecipes] = useState(jsonArray);
- const deleteRecipe = recipeId => { 
- const filteredRecipe = recipes.filter(recipe => recipe.id !== recipeId);
- return setRecipes(filteredRecipe)}
-    return(
+function MainThing({recipes, setRecipes}) {
+ 
+    const deleteRecipe = recipeId => {
+        const filteredRecipe = recipes.filter(recipe => recipe.id !== recipeId);
+        return setRecipes(filteredRecipe)
+    }
+    return (
 
-<div className="Main-Thing">
-    
-   {recipes.map((recipe)=>{
-    return(
-   
-    <div key={recipe.id} className="first-div">
-    <div className="section">
-        
-    <img src={recipe.image} alt="food" />
-    <h3>{recipe.name}</h3>
-    <p>calories: {recipe.calories} </p>
-    <p>Servings: {recipe.servings}</p>
-    <button onClick={()=>deleteRecipe(recipe.id)}>Delete</button>
-    <Link to={`/Details/${recipe.id}`}><button>Details</button></Link>
-    </div>
-</div>
-)})}
-</div>
+        <div className="Main-Thing">
+
+            {recipes.map((recipe) => {
+                return (
+
+                    <div key={recipe.id} className="first-div">
+                        <div className="section">
+
+                            <img src={recipe.image} alt="food" />
+                            <h3>{recipe.name}</h3>
+                            <p>calories: {recipe.calories} </p>
+                            <p>Servings: {recipe.servings}</p>
+                            <button onClick={() => deleteRecipe(recipe.id)}>Delete</button>
+                            <Link to={`/Details/${recipe.id}`}><button>Details</button></Link>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
 
     )
 }
--- src/pages/Detailspage.jsx
+++ src/pages/Detailspage.jsx
@@ -3,23 +3,26 @@ import { useNavigate } from "react-router-dom"
 import RecipeCard from "../Components/RecipeCard"
 import { useParams, Link } from "react-router-dom"
 
-function Details({recipes}){
-    const {recipeId} = useParams()
-    const recipeCard = recipes.find((recipe) =>{
+function Details({ recipes }) {
+    const { recipeId } = useParams();
+
+    const recipeCard = recipes.find((recipe) => {
         return recipe.id === recipeId
     })
-    return(
+
+    console.log(recipes);
+    return (
         <>
-        {
-<div key={recipeCard.id} className="recipe-card">
-<img src={recipeCard.image} alt={recipeCard.name} />
-    <h3>{recipeCard.name}</h3>
-    <p>calories: {recipeCard.calories} </p>
-    <p>Servings: {recipeCard.servings}</p>
-    <p>{recipeCard.preparation}</p>
-    <Link to="/"><button>Go back</button></Link>
-</div>       
-        }
+            {
+                <div key={recipeCard.id} className="recipe-card">
+                    <img src={recipeCard ? recipeCard.image : "https://cdn-icons-png.freepik.com/256/4673/4673720.png"} alt={recipeCard.name} />
+                    <h3>{recipeCard.name}</h3>
+                    <p>calories: {recipeCard.calories} </p>
+                    <p>Servings: {recipeCard.servings}</p>
+                    <p>{recipeCard.preparation}</p>
+                    <Link to="/"><button>Go back</button></Link>
+                </div>
+            }
         </>
     )
 }
